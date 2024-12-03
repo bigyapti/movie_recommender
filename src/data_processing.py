6e717e4e9d0e2e0db46830e0a4242d0a045e9c3b
Bigyapti <bigyapti@Bigyaptis-MacBook.local>
2024-12-03 18:55:54 +0545

    created user-item matrix

--- src/data_processing.py
+++ src/data_processing.py
@@ -1,15 +1,17 @@
 import pandas as pd
 
-movies = pd.read_csv('../data/movies.csv')
-ratings = pd.read_csv('../data/ratings.csv')
+movies = pd.read_csv("../data/movies.csv")
+ratings = pd.read_csv("../data/ratings.csv")
 
 print(movies.head())
 print(ratings.head())
 
-merged_data = pd.merge(ratings, movies, on='movieId')
+merged_data = pd.merge(ratings, movies, on="movieId")
 
 print(merged_data.head())
 
-user_movie_matrix = merged_data.pivot_table(index='userId', columns='movieId', values='rating').fillna(0)
+user_movie_matrix = merged_data.pivot_table(
+    index="userId", columns="movieId", values="rating"
+).fillna(0)
 
 print(user_movie_matrix.head())
